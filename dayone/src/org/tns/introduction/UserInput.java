1193de76e2bd187bcf70c991929ae7d4d5a4c7fb
vishalbodake <vbodke1745@gmail.com>
2023-07-25 13:08:27 +0530

    first commit

---
+++ dayone/src/org/tns/introduction/UserInput.java
@@ -0,0 +1,11 @@
+package org.tns.introduction;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
