80ca3b017078da7378fff04712186ad9d6218153
vishalbodake <vbodke1745@gmail.com>
2023-08-02 10:18:40 +0530

    third commit

---
+++ daythree/src/org/tnsif/classandobject/CitizenExecutor.java
@@ -0,0 +1,27 @@
+
+// This is a driver a class to demonstrate default and parameterized constructor
+package org.tnsif.classandobject;
+
+public class CitizenExecutor {
+
+	public static void main(String[] args) {
+		// object creation
+		Citizen c = new Citizen();   // default constructor is called automatically here
+		
+
+		Citizen c1 = new Citizen("Adult",537845061496L,"Male","Nashik"); 
+		
+		System.out.println(c1);
+		
+		// To string method is used when the no user defined method exits 
+		
+			
+	}
+
+	
+}
+
+	
+	
+
+
